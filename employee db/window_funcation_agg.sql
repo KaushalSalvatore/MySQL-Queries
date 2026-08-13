b69c09c06e33dacd734e655420cb8821ed77d43d
KaushalSalvatore <kaushalpandey455@gmail.com>
2026-08-13 10:23:31 +0530

    window function

--- employee db/window_funcation_agg.sql
+++ employee db/window_funcation_agg.sql
@@ -30,16 +30,52 @@ JOIN projects prj
 ON prj.project_id = eprj.project_id;
 
 -- 4. Calculate the moving average of the last 3 employee salaries based on joining date.
-
+SELECT emp_id, 
+       emp_name,
+       salary, 
+       joining_date, 
+       AVG(salary) OVER(ORDER BY joining_date
+       ROWS BETWEEN 2 PRECEDING AND CURRENT ROW 
+ )  As last_3_month_salary 
+FROM employees
+ORDER BY joining_date;
 
 -- 5. Count number of employees hired till date.
+SELECT emp_id, 
+       emp_name,
+       salary, 
+       joining_date, 
+       COUNT(*) OVER(ORDER BY joining_date
+       ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW 
+ )  As employees_hired_till_dates 
+FROM employees 
+ORDER BY joining_date;
 
 -- 6. Find difference between current and previous row salary.
+SELECT
+emp_name, salary, LAG(salary) over(
+    ORDER by joining_date 
+) AS previous_salary,
+salary - LAG(salary) OVER(
+    ORDER BY joining_date
+) AS salary_difference
+FROM employees
+ORDER BY joining_date;
 
 -- 7. Find maximum salary till current row.
+SELECT emp_name, salary, joining_date
+MAX(salary) OVER(ORDER BY joining_date ROWS BETWEEN UNBOUNDED
+PRECEDING AND CURRENT ROW) AS maximum_salary
+FROM employees
+ORDER BY joining_date;
 
 -- 8. Calculate rolling 7-day average.
+select emp_id, AVG(salary) OVER(ORDER BY joining_date RANGE BETWEEN INTERVAL '6 days' PRECEDING AND CURRENT ROW) AS 7_day_avg
+FROM employees 
+ORDER BY joining_date;
+
 
 -- 9. Show percentage contribution of each employee salary.
+
 
 -- 10. Find running sum reset per department.
