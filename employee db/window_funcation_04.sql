1779dfc4307abc01c31e1a0b74c2dadd148bbbff
KaushalSalvatore <kaushalpandey455@gmail.com>
2026-09-12 23:38:47 +0530

    windows functions

--- employee db/window_funcation_04.sql
+++ employee db/window_funcation_04.sql
@@ -1,39 +1,33 @@
--- Find latest transaction per customer.
+--1. Find latest transaction per customer.
 SELECT customer_name , tran_id , trans_date 
 from customer 
-QUALIFY ROW_NUMBER() OVER(PARTITION BY cus_id , ORDER BY trans_id DESC) = 1;
+QUALIFY ROW_NUMBER() OVER(PARTITION BY cus_id , ORDER BY trans_id DESC);
 
--- Remove duplicate rows using window functions.
-
-
--- Identify duplicate records based on multiple columns.
+--2. Remove duplicate rows using window functions.
 
 
--- Find employees who joined on same day.
+--3. Identify duplicate records based on multiple columns.
 
 
--- Detect islands and gaps problem.
+--4. Find employees who joined on same day.
 
 
--- Find continuous login days.
+--5. Detect islands and gaps problem.
 
 
--- Find customers with consecutive purchases.
+--6. Find continuous login days.
 
 
--- Rank rows based on multiple columns.
+-- 7. Find customers with consecutive purchases.
 
 
--- FRAME CLAUSE (INTERVIEWER FAVORITE)
+--8. Rank rows based on multiple columns.
 
 
--- Calculate sliding window sum (3 previous rows).
+--9. FRAME CLAUSE (INTERVIEWER FAVORITE)
+
+
+--10 Calculate sliding window sum (3 previous rows).
 
 
 -- Calculate moving average including current + next rows.
-
-
--- Why does LAST_VALUE() give unexpected output?
-
-
--- How to fix LAST_VALUE() using frame clause?
